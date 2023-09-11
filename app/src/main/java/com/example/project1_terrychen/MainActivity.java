22fcd687afb58b4f322df4007afbfefd5ff58f20
chenterry85 <chenterry85@gmail.com>
2023-09-11 11:20:45 -0700

    Add Button toggle logic

--- app/src/main/java/com/example/project1_terrychen/MainActivity.java
+++ app/src/main/java/com/example/project1_terrychen/MainActivity.java
@@ -4,19 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 	private int elapsedTimeInSeconds = 0;
 	private boolean isGameOngoing = true;
+	private boolean isPickaxeMode = true;
 
-    @Override
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		startGameTimer();
 
+	}
+
+	public void handlePickaxeModeToggle(View view) {
+		isPickaxeMode = !isPickaxeMode;
+		TextView pickButton = findViewById(R.id.pickButton);
+		pickButton.setText(getString(isPickaxeMode ? R.string.pick : R.string.flag));
 	}
 
 	private void startGameTimer() {
