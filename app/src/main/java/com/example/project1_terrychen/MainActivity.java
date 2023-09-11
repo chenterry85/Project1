1088c41dbe1015556976f3e3773c02b9323839e7
chenterry85 <chenterry85@gmail.com>
2023-09-11 11:08:20 -0700

    Add Timer logic

--- app/src/main/java/com/example/project1_terrychen/MainActivity.java
+++ app/src/main/java/com/example/project1_terrychen/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.project1_terrychen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private int elapsedTimeInSeconds = 0;
+	private boolean isGameOngoing = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+		startGameTimer();
+
+	}
+
+	private void startGameTimer() {
+		TextView timerTextView = findViewById(R.id.clockTimer);
+		Handler handler = new Handler();
+		handler.post(new Runnable() {
+			@Override
+			public void run() {
+				if (isGameOngoing) {
+					elapsedTimeInSeconds++;
+					timerTextView.setText(String.valueOf(elapsedTimeInSeconds));
+				}
+				handler.postDelayed(this, 1000);
+			}
+		});
+	}
 }
